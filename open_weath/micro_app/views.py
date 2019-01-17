54bb9b72bc71e71b106a1cf27fb14ac039d883b3
reedmen15 <44979055+reedmen15@users.noreply.github.com>
2019-01-17 17:27:58 +0200

    next_step

--- open_weath/micro_app/views.py
+++ open_weath/micro_app/views.py
@@ -1,28 +1,37 @@
 from django.shortcuts import render
 import json
 import requests
-#from django.http import HttResponse
+#from .forms import getCity
+#from .models import byCity
 
-# Create your views here.
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=404d7b80e842155d20b3d2c3f1a0604c'
     
-    city = 'Kiev'
-       
+    
+    n = []
+    if request.method == 'POST':
+        form = request.POST.get('inputCity')
+        n.append(form)
+
+    if n == n:
+        city = n
+    else:
+        city = 'kiev'
+    
+    print(city)
     weather_data = []
-
-    r = requests.get(url.format(city)).json()
-
-    city_weather = {
-        'city' : city,
-        'temperature' : r['main']['temp'],
-        'description' : r['weather'][0]['description'],
-        'icon' : r['weather'][0]['icon'],
-    }
-
-    weather_data.append(city_weather)
+    for city in n:
+        r = requests.get(url.format(city)).json()
+    
+        city_weather = {
+            'city' : city,
+            'temperature' : r['main']['temp'],
+            'description' : r['weather'][0]['description'],
+            'icon' : r['weather'][0]['icon'],
+        }
+    
+        weather_data.append(city_weather)
 
     context = {'weather_data' : weather_data}
     return render(request, 'micro_app/index.html', context)
-
         
